5206123490efb85aad0cb65bccf4375b655094bb
Mauricio Parrado P <mauricio.parrado@acm.org>
2025-10-03 13:22:46 -0400

    2-8 finished.

--- part_one/ch_2/2-9_number8.py
+++ part_one/ch_2/2-9_number8.py
@@ -1,12 +1,29 @@
 import random
-operatiors = ['+','-','*','/']
+operators = ['+','-','*','/']
+data = []
 
 a=0
 b=0
+output=0
+iterations = 0
+c = random.randint(1,50)
 
-for operator in operatiors:
-    output = int(a + operator + b)
-    while output != 8:
-        a,b = random.randint(1,50)
-        print(f'a={a}, b={b}')
-        print(f'The result is: {output}')
+for operation in operators:
+    while output != c:
+        a = random.randint(1,50)
+        b = random.randint(1,50)
+        calculation = str(a) + operation + str(b)
+        output = eval(calculation)
+        #print(f'a={a}, b={b}\t {operation}')
+        #print(f'This time the result is: {output}')
+        iterations +=1
+    
+    #print(f'The result is: {output}, it took {iterations} evaluation(s) to get {c}.\nMoving to the next operator...')
+    data.append(f'({operation}):{a}{operation}{b}={output}, it took {iterations} evaluation(s) to get {c}.')
+    
+    output=0
+    iterations =0
+
+print(data)
+
+

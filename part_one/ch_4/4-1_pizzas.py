58b9f07b70232de7f3f83f06c69847aaec0b57f2
Mauricio P <mauricio.parrado@acm.org>
2025-10-08 21:03:11 -0400

    folder ordered. ch_4 started

---
+++ part_one/ch_4/4-1_pizzas.py
@@ -0,0 +1,10 @@
+#think of at least three kinds of your favorite pizza. Store these pizza names in a list, 
+# and then use a for loop to print the name of each pizza.
+
+pizzas = ['hawaian', 'burrata', 'pepperoni', 'four cheese']
+
+for pizza in pizzas:
+    print(f'A {pizza.title()} has been added to the order')
+
+print(f'A total of {len(pizzas)} pizzas were ordered') #add a line at the end of your program, outside the for loop
+

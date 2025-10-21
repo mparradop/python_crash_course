3d87e399d3bc92441f9a2e59d65089ece690bd82
Mauricio P <mauricio.parrado@acm.org>
2025-10-21 19:04:32 -0400

    4-10 created

---
+++ part_one/ch_4/4-10_slices.py
@@ -0,0 +1,2 @@
+#using one of the programs you wrote in this chapter, add several lines to the end of the program that do the following
+#print the message 'the first three items in the list are:. Then use a slice to print the first three items from taht program's list

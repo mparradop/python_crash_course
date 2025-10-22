3a9ab99739ffb1427b19b3dc295bca8f751bc227
Mauricio Parrado P <mauricio.parrado@acm.org>
2025-10-21 22:48:18 -0400

    ch_5. conditionals

---
+++ part_one/ch_5/5-1_conditional_tests.py
@@ -0,0 +1,9 @@
+"""
+write a series of conditional tests. Print a statement descibing each test and your prediction for the results of each set.
+"""
+car = 'volkswagen'
+print(f"Is car == 'volkswagen'? I predict True.")
+print(car=='volkswagen')
+
+print("\nIs car == 'audi'? I predict False.")
+print(car=='audi')
